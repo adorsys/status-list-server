3458273f807632fbe7093e72789f532c5e0e47ab
christian_yemele <yemelechristian2@gmail.com>
2025-03-03 22:22:19 +0100

    fix(database): allow clippy lint for enum variant names in RepositoryError

--- src/database/error.rs
+++ src/database/error.rs
@@ -1,6 +1,7 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
+#[allow(clippy::enum_variant_names)]
 pub enum RepositoryError {
     #[error("could not store entity")]
     StoreError,
@@ -9,5 +10,5 @@ pub enum RepositoryError {
     #[error("could not update entity")]
     UpdateError,
     #[error("could not delete entity")]
-    DeleteError
+    DeleteError,
 }
