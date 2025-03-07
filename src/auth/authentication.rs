50f248031f34b29accb76a154bf0e94cad58355d
Christian_Yemele <yemelechristian2@gmail.com>
2025-03-07 13:18:32 +0100

    feat(auth): enhance JWT credential publishing with issuer validation and algorithm checks

--- src/auth/authentication.rs
+++ src/auth/authentication.rs
@@ -4,15 +4,38 @@ use crate::{
     utils::state::AppState,
 };
 
+use super::errors::AuthErrors;
+
 /// Handle JWT registration
 pub async fn publish_credentials(
     credentials: Credentials,
     state: AppState,
 ) -> Result<(), RepositoryError> {
-    // try storing connection with token issuer
-    if let Some(store) = state.repository {
-        Ok(store.credential_repository.insert_one(credentials).await?)
-    } else {
-        Err(RepositoryError::RepositoryNotSet)?
+    // Ensure the repository is available
+    let store = state.repository.ok_or(RepositoryError::RepositoryNotSet)?;
+
+    // Check if the issuer already exists
+    if store
+        .credential_repository
+        .find_one_by(credentials.issuer.clone())
+        .await?
+        .is_some()
+    {
+        return Err(RepositoryError::DuplicateEntry);
     }
+
+    // Validate the algorithm
+    let algorithm = credentials.alg.clone();
+    match algorithm.as_str() {
+        "RS256" | "RS384" | "RS512" | "ES256" | "ES384" | "ES512" => (),
+        _ => return Err(RepositoryError::from(AuthErrors::UnknownAlgorithm))?,
+    }
+
+    // ensure consistent order in credentials
+    let credential = Credentials::new(credentials.issuer, credentials.public_key, credentials.alg);
+
+    // Insert the credentials into the repository
+    store.credential_repository.insert_one(credential).await?;
+
+    Ok(())
 }
