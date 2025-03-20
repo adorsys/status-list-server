f82ca72010aa985933f85e45a897252e42acd7a9
Hermann Core <hermannoenpause@gmail.com>
2025-03-20 17:08:20 +0100

    add endpoint for status list retrieval

--- src/web/handlers.rs
+++ src/web/handlers.rs
@@ -4,5 +4,5 @@ mod status_list;
 pub use credential_issuance::credential_handler;
 pub use status_list::{
     error::StatusListError,
-    handler::{handle_status_list_retrieval, update_statuslist},
+    handler::{get_status_list, update_statuslist},
 };
