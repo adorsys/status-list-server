e008d961b8fdfe0467dac43306ed7fef85677424
Hermann <hermannoenpause@gmail.com>
2025-04-08 12:02:28 +0100

    feat: implement key generation for signing token

--- src/utils/keygen.rs
+++ src/utils/keygen.rs
@@ -1,34 +1,64 @@
-use rsa::{RsaPrivateKey, RsaPublicKey};
+use ed25519_dalek::{
+    pkcs8::{spki::der::pem::LineEnding, EncodePrivateKey},
+    SigningKey, VerifyingKey, SECRET_KEY_LENGTH,
+};
+use rand::{rngs::OsRng, RngCore};
+use serde::{Deserialize, Serialize};
 
 use super::errors::Error;
 
+/// A keypair for signing and verifying JWT
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Keypair {
-    pub private_key: RsaPrivateKey,
-    pub public_key: RsaPublicKey,
+    repr: KeyRepr,
 }
 
-/// This function should be called once to generate a new keypair
-pub fn keygen() -> Result<Keypair, Error> {
-    let mut rng = rand::thread_rng();
-    let bits = 2048;
-    let priv_key = RsaPrivateKey::new(&mut rng, bits)
-        .map_err(|_| Error::Generic("failed to generate key".to_string()))?;
-    let pub_key = RsaPublicKey::from(&priv_key);
+#[derive(Debug, Serialize, Deserialize)]
+struct KeyRepr {
+    signing_key: SigningKey,
+    verifying_key: VerifyingKey,
+}
 
-    let keypair = Keypair {
-        private_key: priv_key,
-        public_key: pub_key,
-    };
-    Ok(keypair)
+impl Keypair {
+    /// Generate a new random keypair
+    pub fn generate() -> Result<Self, Error> {
+        let mut seed = [0u8; SECRET_KEY_LENGTH];
+        OsRng
+            .try_fill_bytes(&mut seed)
+            .map_err(|_| Error::KeyGenFailed)?;
+        let signing_key = SigningKey::from_bytes(&seed);
+        let verifying_key = signing_key.verifying_key();
+
+        let keypair = Keypair {
+            repr: KeyRepr {
+                signing_key,
+                verifying_key,
+            },
+        };
+        Ok(keypair)
+    }
+
+    /// Convert the private key to a pkcs8 PEM string
+    pub fn to_pkcs8_pem(&self) -> Result<String, Error> {
+        self.repr
+            .signing_key
+            .to_pkcs8_pem(LineEnding::default())
+            .map_err(|_| Error::PemGenFailed)
+            .map(|pem| pem.to_string())
+    }
+
+    /// Convert the private key to a pkcs8 PEM bytes
+    pub fn to_pkcs8_pem_bytes(&self) -> Result<Vec<u8>, Error> {
+        self.to_pkcs8_pem().map(|pem| pem.into_bytes())
+    }
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+    use ed25519_dalek::pkcs8::EncodePublicKey;
     use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
-    use rsa::pkcs1::{EncodeRsaPrivateKey, EncodeRsaPublicKey};
     use std::time::{SystemTime, UNIX_EPOCH};
-
-    use crate::utils::keygen::keygen;
 
     #[test]
     fn test() {
@@ -37,21 +67,15 @@ mod test {
             exp: usize,
         }
 
-        let keypair = keygen().unwrap();
+        let keypair = Keypair::generate().unwrap();
 
         let header = Header {
-            alg: jsonwebtoken::Algorithm::RS256,
+            alg: jsonwebtoken::Algorithm::EdDSA,
             ..Default::default()
         };
 
-        let encoding_key = EncodingKey::from_rsa_pem(
-            keypair
-                .private_key
-                .to_pkcs1_pem(rsa::pkcs8::LineEnding::CRLF)
-                .unwrap()
-                .as_bytes(),
-        )
-        .unwrap();
+        let encoding_key =
+            EncodingKey::from_ed_pem(&keypair.to_pkcs8_pem_bytes().unwrap()).unwrap();
 
         let exp_time = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -63,15 +87,16 @@ mod test {
         let token = encode(&header, &claims, &encoding_key).unwrap();
 
         let public_key_pem = keypair
-            .public_key
-            .to_pkcs1_pem(rsa::pkcs8::LineEnding::CRLF)
+            .repr
+            .verifying_key
+            .to_public_key_pem(LineEnding::default())
             .unwrap();
-        let decoding_key = DecodingKey::from_rsa_pem(public_key_pem.as_bytes()).unwrap();
+        let decoding_key = DecodingKey::from_ed_pem(public_key_pem.as_bytes()).unwrap();
 
         let decoded = decode::<Claims>(
             &token,
             &decoding_key,
-            &Validation::new(jsonwebtoken::Algorithm::RS256),
+            &Validation::new(jsonwebtoken::Algorithm::EdDSA),
         );
 
         assert!(decoded.is_ok(), "Decoding failed: {:?}", decoded.err());
