f3c2568cd689789f513681803e339cd21ca4cfd4
Hermann <hermannoenpause@gmail.com>
2025-04-17 17:02:56 +0100

    feat: implement JWT and CWT issuance for status list tokens

--- src/web/handlers/status_list/constants.rs
+++ src/web/handlers/status_list/constants.rs
@@ -1,2 +1,4 @@
-pub(super) const STATUS_LISTS_HEADER_JWT: &str = "application/statuslist+jwt";
-pub(super) const STATUS_LISTS_HEADER_CWT: &str = "application/statuslist+cwt";
+pub(super) const ACCEPT_STATUS_LISTS_HEADER_JWT: &str = "application/statuslist+jwt";
+pub(super) const ACCEPT_STATUS_LISTS_HEADER_CWT: &str = "application/statuslist+cwt";
+pub(super) const STATUS_LISTS_HEADER_JWT: &str = "statuslist+jwt";
+pub(super) const STATUS_LISTS_HEADER_CWT: &str = "statuslist+cwt";
