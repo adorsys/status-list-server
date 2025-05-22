8fddc6d1385532d8b3eef110c7274556c673ff62
Boris <borisngha6@gmail.com>
2025-05-22 15:33:53 +0100

    fix: resolving conflicts

--- src/test_resources/helper.rs
+++ src/test_resources/helper.rs
@@ -1,6 +1,6 @@
-use crate::model::StatusRequest;
 #[cfg(test)]
-use crate::{model::StatusEntry, utils::keygen::Keypair};
+use crate::model::StatusEntry;
+use crate::model::StatusRequest;
 
 // Helper to create a test request payload with customizable bits
 #[cfg(test)]
@@ -9,11 +9,4 @@ pub fn publish_test_token(list_id: &str, status: Vec<StatusEntry>) -> StatusRequ
         list_id: list_id.to_owned(),
         status,
     }
-}
-
-// Helper to generate a test server key
-// Note: It does nothing, it's just use to build the AppState
-#[cfg(test)]
-pub fn server_key() -> Keypair {
-    Keypair::generate().unwrap()
 }
